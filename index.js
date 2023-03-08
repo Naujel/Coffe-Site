f955a204f870b9bc0d5e3d67dd3b94a6631ac261
Naujel <113215048+Naujel@users.noreply.github.com>
2023-03-08 18:30:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { elementsScroll } from "./elements-scroll.js";
 import { navbarScroll } from "./navbar-scroll.js";
 import { scrollSpy } from "./scroll-spy.js";
 import { carrusel } from "./carrusel.js";
+import { validarFormulario } from "./formulario.js";
+import { menuCarro } from "./menu-carro.js";
 
 const secciones = document.querySelectorAll("section")
 const enlaces = document.querySelectorAll(".enlace")
@@ -10,6 +12,8 @@ window.addEventListener("load", () => {
     scrollSpy(secciones, enlaces)
     navbarScroll()
     //bannerShow()
-    elementsScroll(`.img-container, .text-container, .banner, .slider, .text-container2`)
+    elementsScroll(`.img-container, .text-container, .banner, .slider, .text-container2, .mapa-ubicacion, .formulario`)
     carrusel()
+    validarFormulario()
+    menuCarro()
 })
